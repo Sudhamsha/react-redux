db1374da3c1097830486a1573cb445b59409e41c
Sudhamsha <sudhamshareddy@gmail.com>
2017-08-16 20:07:53 +0100

    Initial Commit with a basic todo list

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoForm from './components/todoform';
+import TodoList from './components/TodoList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Hello World!</h2>
+        </div>
+
+          <div className="todo-list">
+              <TodoForm
+                  currentTodo = {this.props.currentTodo}
+                  changeCurrent = {this.props.changeCurrent}
+              />
+             <TodoList todos={this.props.todos}/>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/todo.js
@@ -0,0 +1,24 @@
+const initState = {
+    todos: [
+        {id: 1, name: 'Create Static UI', isComplete: true},
+        {id: 2, name: 'Create Initial State', isComplete: true},
+        {id: 3, name: 'Use State to render UI', isComplete: true},
+    ],
+    currentTodo: ''
+}
+
+const CURRENT_UPDATE = 'CURRENT_UPDATE'
+const TODO_ADD = 'TODO_ADD'
+
+export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val})
+
+export default (state = initState, action) => {
+    switch (action.type){
+        case TODO_ADD:
+            return {...state, todos: state.todos.concat(action.payload)}
+        case CURRENT_UPDATE:
+            return {...state, currentTodo: action.payload}
+        default:
+            return state
+    }
+}
