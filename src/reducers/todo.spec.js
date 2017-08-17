d91749dbca4196825f1d237b3785efeab65d17fc
Sudhamsha <sudhamshareddy@gmail.com>
2017-08-17 19:59:08 +0100

    Updated Test case

--- src/reducers/todo.spec.js
+++ src/reducers/todo.spec.js
@@ -20,7 +20,8 @@ describe('Todo Reducer', () => {
                 {id: 2, name: 'Create Initial State', isComplete: true},
                 {id: 3, name: 'Use State to render UI', isComplete: true},
                 {id: 4, name: 'Added todo', isComplete: false},
-            ]
+            ],
+            currentTodo: ''
         }
 
         const action = {type: 'TODO_ADD', payload: {id: 4, name: 'Added todo', isComplete: false}}
