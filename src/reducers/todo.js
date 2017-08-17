5a1ee1b205aedf87f32d5671fd4022da2b9291ce
Sudhamsha <sudhamshareddy@gmail.com>
2017-08-17 11:57:37 +0100

    Save record to the server (mockup)

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,4 +1,4 @@
-import {getTodos} from '../lib/todoService'
+import {getTodos, createTodo} from '../lib/todoService'
 
 const initState = {
     todos: [],
@@ -11,6 +11,7 @@ const TODO_ADD = 'TODO_ADD'
 
 export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val})
 export const loadTodos = (todos) => ({type: TODO_LOAD, payload: todos})
+export const addTodo = (todo) => ({type: TODO_ADD, payload: todo})
 
 export const fetchTodos = () => {
     return (dispatch) => {
@@ -19,10 +20,19 @@ export const fetchTodos = () => {
     }
 }
 
+export const saveTodo = (name) => {
+    return (dispatch) => {
+        createTodo(name)
+            .then(res => dispatch(addTodo(res)))
+    }
+}
+
+
+
 export default (state = initState, action) => {
     switch (action.type){
         case TODO_ADD:
-            return {...state, todos: state.todos.concat(action.payload)}
+            return {...state, currentTodo: '', todos: state.todos.concat(action.payload)}
         case TODO_LOAD:
             return {...state, todos: action.payload}
         case CURRENT_UPDATE:
