b4f9564332cdf6cce6e3874346a0dab8fb728568
Sudhamsha <sudhamshareddy@gmail.com>
2017-08-17 13:24:38 +0100

    Added basic filtering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from './components/todoform';
 import TodoList from './components/TodoList';
 import Message from './components/Message';
+import Footer from './components/Footer';
 
 
 class App extends Component {
@@ -14,12 +16,18 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Hello World!</h2>
         </div>
+          <Router>
+              <div className="todo-list">
+                  <Message />
+                  <TodoForm />
 
-          <div className="todo-list">
-              <Message />
-              <TodoForm />
-              <TodoList />
-          </div>
+                  <Route path='/:filter?' render={({match}) => (
+                      <TodoList filter={match.params.filter} />
+                  )}/>
+                  <Footer />
+              </div>
+          </Router>
+
       </div>
     );
   }
